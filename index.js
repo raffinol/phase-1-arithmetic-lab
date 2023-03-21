099a821b48608d2bf5b274a30cfbbfb9a784fec1
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-21 16:41:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 19;
+let num4 = 5;
+let varMax = (5, 10, 15, 20);
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4
+let max = Math.max(varMax);
+console.log(max)
+
+
